acbfbe3ea58dc5dfeedf0397a103000951f689a5
Jason Rial <monahajt@gmail.com>
2017-12-05 20:40:57 -0600

    initial commit with 3d plane and player movement with camera movement

---
+++ fps-unity/Assets/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float lookSensitivity = 3f;
+
+    private PlayerMotor motor;
+
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    void Update()
+    {
+        //Calculate movement velocity as 3d vector
+        float _xMov = Input.GetAxisRaw("Horizontal");
+        float _zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 _movHorizontal = transform.right * _xMov;
+        Vector3 _movVertical = transform.forward * _zMov;
+
+        //final movement vector
+        Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
+
+        //Apply movement
+        motor.Move(_velocity);
+
+        //calculate rotation as a 3d vector: (turning around)
+        float _yRot = Input.GetAxisRaw("Mouse X");
+        Vector3 _rotation = new Vector3(0f, _yRot, 0f) * lookSensitivity;
+
+        //apply rotation
+        motor.Rotate(_rotation);
+
+        //calculate camera rotation as a 3d vector: (turning around)
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+        Vector3 _cameraRotation = new Vector3(_xRot, 0f, 0f) * lookSensitivity;
+
+        //apply camera rotation
+        motor.RotateCamera(_cameraRotation);
+    }
+}
